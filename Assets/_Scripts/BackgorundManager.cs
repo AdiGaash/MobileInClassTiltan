375da731b6d8a7f9474b41f0ce3cfe14a24b4be1
AG <adigaash@gmail.com>
2025-09-07 17:13:17 +0300

    Add level manager that will become scriptable

---
+++ Assets/_Scripts/BackgorundManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class BackgorundManager : MonoBehaviour
+    {
+        public LevelManager levelManager;
+        public float CloudSpeedModifier = 1f;
+        public float LandSpeedModifier = 1f;
+        public float outOfScreenBoundX = 10f;
+        public float outOfScreenBoundZ = 10f;
+        public CameraBoundsController cameraBoundsController;
+        [HideInInspector]
+        public Rect activeAreaBounds;
+        [HideInInspector]
+        public Rect spawnAreaBounds;
+       
+        
+
+        private void OnEnable()
+        {
+            cameraBoundsController.OnBoundariesChanged += UpdatePoolArea;
+        }
+        private void OnDisable()
+        {
+            cameraBoundsController.OnBoundariesChanged -= UpdatePoolArea;
+        }
+
+        void UpdatePoolArea(Rect newBoundaries)
+        {
+            activeAreaBounds = cameraBoundsController.CalculateBoundaries(outOfScreenBoundX,outOfScreenBoundZ);
+            spawnAreaBounds = new Rect(newBoundaries.xMin - outOfScreenBoundX, newBoundaries.height,
+                newBoundaries.width + outOfScreenBoundX, newBoundaries.height + outOfScreenBoundZ);
+        }
+
+        private void Start()
+        {
+            Invoke("SpawnCloud",CloudSpeedModifier+levelManager.GameSpeed);
+            Invoke("SpawnOnLand",LandSpeedModifier+levelManager.GameSpeed);
+        }
+
+        private void Update()
+        {
+            SpawnCloud();
+            SpawnOnLand();
+            
+        }
+
+        private void SpawnOnLand()
+        {
+            
+        }
+
+        private void SpawnCloud()
+        {
+            
+        }
+    }
+}
---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Shooter
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public float GameSpeed = 3f;
+        public AudioClip backgroundMusic;
+    }
+}
