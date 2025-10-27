d55c6ffbc4bcf2a04a3bdd8050d15cf4f4cc7ec9
AG <adigaash@gmail.com>
2025-10-27 15:09:03 +0200

    last push of lesson from 20 Oct

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class GameManager: Singleton<GameManager>
+    {
+        public LevelParameters LevelParameters;
+    }
+
+
+    public class GameLogic: MonoBehaviour
+    {
+        private void Start()
+        {
+            GameManager.Instance.LevelParameters = new LevelParameters();
+        }
+    }
+}
