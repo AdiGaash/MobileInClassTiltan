b07699ae1d4dfea883a2e25168c689e90e71d21c
AG <adigaash@gmail.com>
2025-11-11 16:47:55 +0200

    set bullet param range

--- Assets/_Scripts/ShootingParameters.cs
+++ Assets/_Scripts/ShootingParameters.cs
@@ -6,12 +6,17 @@ namespace Shooter
     public class ShootingParameters : ScriptableObject
     {
         [Header("Fire Settings")]
+        [Range(0.2f, 5f)]
         public float fireRate = 0.5f;
+        [Range(0.1f, 4f)]
         public float bulletDamage = 10f;
+        [Range(5f, 30f)]
         public float bulletSpeed = 20f;
+        [Range(1, 3)]
         public int numOfBulletsPerShot = 1;
         [Header("Bullet Properties")]
         public GameObject bulletPrefab;
+        [Range(0.1f, 10f)]
         public float bulletLifetime = 3f;
         
     }
